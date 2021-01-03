780d5729e84afed060c2ddab1f89e0b73f07e94d
Nhajela <hajela.naman@gmail.com>
2021-01-03 16:51:32 +0530

    docs/refactors readablity cleanup and comments added

--- script.js
+++ script.js
@@ -5,34 +5,34 @@ const twitterButton = document.getElementById("twitter")
 const quoteButton = document.getElementById("new-quote")
 const loader = document.getElementById("loader")
 
-
-function loading(){
+function showLoadingSpinner(){
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
-function complete(){
+function removeLoadingSpinner(){
    if (!loader.hidden) {
        loader.hidden = true;
        quoteContainer.hidden = false;
    }
 }
-// get quote from api
 
+// get quote from api + add it to DOM
 async function getQuote() {
     const proxyUrl = "https://tranquil-garden-89684.herokuapp.com/"
     const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 
     try {
-        loading();
+       showLoadingSpinner();
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
 
-        complete();
         // assigning it to DOM
 
         authorText.innerText = data.quoteAuthor ? data.quoteAuthor : "unknown";
 
+
+        // dynamically reduce font size for longer quotes. 
         if (data.quoteText.length > 120) {
             quoteText.classList.add("long-quote");
         }
@@ -41,7 +41,11 @@ async function getQuote() {
         }
 
         quoteText.innerText = data.quoteText;
+
+        removeLoadingSpinner();
     }
+
+    // infinite loop caution - recursive code if try block throws any error.
     catch (error) {
         getQuote();
         console.log(error);
